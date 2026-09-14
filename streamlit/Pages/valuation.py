09eff016850d3b22631b3d09c8fea5b34b3952fb
Piexgear <davidpuscas@live.se>
2026-09-14 12:12:06 +0200

    Worked on randomforest and got some sucess.
    The RMSE for random is better than linear with full data.
    im going to evaluat rf with full data and see what it says.
    
    Also cleaned up a bit.
    
    I also removed the Price (SEK) from x because it was as we suspected that
    it got overfitted....

--- streamlit/Pages/valuation.py
+++ streamlit/Pages/valuation.py
@@ -6,6 +6,87 @@ import pandas as pd
 st.title("Valuat your MC")
 st.write("Please fill in the information needed to see the value on your MC")
 
+brand = st.selectbox(
+    "Brand",
+    [
+        "Benelli",
+        "Husqvarna",
+        "Mutt Motorcycles",
+        "Harley-Davidson",
+        "KTM",
+        "Triumph",
+        "Genuine Scooters",
+        "BMW",
+        "Beta",
+        "Honda",
+        "Royal Enfield",
+        "GasGas",
+        "TVS",
+        "Kawasaki",
+        "Moto Guzzi",
+        "Suzuki",
+        "Yamaha",
+        "Bajaj",
+        "Hero",
+        "Jawa",
+        "Keeway",
+        "Aprilia",
+        "Brixton Motorcycles",
+        "Buell Motorcycle Company",
+        "Cagiva",
+        "Ducati",
+        "Indian",
+        "Mahindra",
+        "MV Agusta",
+        "Piaggio",
+        "SYM",
+        "Vespa",
+        "Sherco",
+        "Victory",
+        "Hanway",
+        "Zero Motorcycles",
+        "Sinnis",
+        "SWM",
+        "Super Soco",
+        "Energica Motor Company",
+        "CCM",
+        "CFMOTO",
+        "Daelim",
+        "Kymco",
+        "Leonart",
+        "Lifan",
+        "Norton",
+        "Moto Morini",
+        "Vmoto Soco",
+        "Lexmoto",
+        "Mash",
+        "Aeon",
+        "UM Motorcycles",
+        "NIU",
+        "Royal Alloy",
+        "Zontes",
+        "Confederate",
+        "Peugeot",
+        "FB Mondial",
+        "Can-Am",
+        "Scomadi",
+        "AJS",
+        "Lambretta",
+        "Triton Electric Bikes",
+        "Apollo",
+        "Hero Electric",
+        "Ampere",
+        "Ontrack",
+        "Derbi",
+        "Italjet",
+        "Segway",
+        "Arcfox",
+        "Hyosung",
+        "Fantic",
+        "Larry vs Harry",
+    ]
+)
+
 Looks = st.selectbox(
     "Looks",
     ["Modern ","Classic", "Sport", "Adventure", "Retro", "Urban", "Off-road", "Cruiser", "Practical"]
@@ -18,21 +99,35 @@ bt = st.selectbox(
 
 Year = st.number_input("Year", min_value=1960, max_value=2026, value=1960)
 
-hp = st.selectbox(
-    "Horse Power",
-    ["<50", "<100", "<150", "Much power as possible"]
-)
+torque = st.number_input("Torque", min_value=3, max_value=301, value=3)
+
+num_of_cc = st.number_input("CC", min_value=7, max_value=2500, value=7)
+
+hp = st.number_input("Horse Power", min_value=10, max_value=600, value=10)
+
 nos = st.selectbox(
     "Number of seats",
     [1, 2]
 )
 
+number_of_cylinder = st.selectbox(
+    "Number of cylinders",
+    [1, 2, 3, 4]
+)
+
+price = 1
+
 Valuation = pd.DataFrame({
-        "Year": [Year],
+        "Company": [brand],
         "Looks": [Looks],
         "Body Type": [bt],
+        "Year": [Year],
+        "Torque": [torque],
+        "Number of cc": [num_of_cc],
         "Horsepower": [hp],
-        "Number of Seating": [nos]
+        "Number of Seating": [nos],
+        "Number of Cylinders": [number_of_cylinder],
+        "Price (SEK)": price
     })
 
 @st.cache_resource
@@ -47,7 +142,37 @@ st.success("Model loaded successfuly!")
 
 if st.button("Predict"):
     prediction = model.predict(Valuation)
-    st.success("Predikterad värde för : ", Valuation)
+    st.success(f"Predikterat värde: {prediction[0]:,.0f} SEK")
 
 
-# ValueError: columns are missing: {'Price (SEK)', 'Horsepower', 'Number of Seating', 'Number of Cylinders', 'Company', 'Torque', 'Number of cc'}
+
+#Naked/Street
+
+# Year
+
+# 2006
+
+
+# Torque
+
+# 75
+
+
+# CC
+
+# 750
+
+
+# Horse Power
+
+# 110
+
+
+# Number of seats
+
+# 2
+
+# Number of cylinders
+
+# 4
+
