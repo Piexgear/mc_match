d19db627e43c6be232ad3267b5c7bb9b13e9d5ff
Piexgear <davidpuscas@live.se>
2026-09-17 15:53:20 +0200

    made some style changes

--- streamlit/Pages/home.py
+++ streamlit/Pages/home.py
@@ -1,36 +1,70 @@
 import streamlit as st
-from app import match, valuation
 
 st.title("MC Match")
-st.write("This page will match you with your next mc with our machine learned model!")
-st.write("There is also a feature for valuting your current mc")
-
+st.write("This page will match you with your next MC with our machine learned model!")
+st.write("There is also a feature for valuating your current MC.")
 
 col1, divider, col2 = st.columns([1, 0.05, 1])
 
-with col1:
+st.markdown("""
+    <style>
+    /* Visa divider på större skärmar */
+    .divider {
+        border-left: 2px solid #ccc;
+        height: 300px;
+        margin: auto;
+    }
+
+    /* Dölj divider på mindre skärmar */
+    @media (max-width: 768px) {
+        .divider {
+            display: none;
+        }
+    }
+    </style>
+""", unsafe_allow_html=True)
+
+
+with col2:
     st.markdown(
         """
-        This is our classification model that will estemate what type of mc that matches you.
-        you will enter some variables that will get sent to the Random forest classifier model 
+        ### Match
+
+        This is our classification model that will estimate
+        what type of MC matches you.
+        You will enter some variables that will get sent
+        to our Random Forest classifier model.
         """
     )
-    
-    if st.button("Valuation"):
-        st.switch_page(valuation)
+
+    button_left, button, button_right = st.columns([1, 2, 1])
+
+    with button:
+        if st.button("Match", use_container_width=True):
+            st.switch_page("pages/match.py")
+
 
 with divider:
     st.markdown(
         """
-        <div style="
-            border-left: 2px solid #ccc;
-            height: 300px;
-            margin: auto;
-        "></div>
+        <div class="divider"></div>
         """,
         unsafe_allow_html=True
     )
 
-with col2:
-    if st.button("Match"):
-        st.switch_page(match)
+
+with col1:
+    st.markdown(
+        """
+        ### Valuation
+
+        This is our valuation model. You will enter some
+        variables about your MC and get an estimated value.
+        """
+    )
+
+    button_left, button, button_right = st.columns([1, 2, 1])
+
+    with button:
+        if st.button("Valuation", use_container_width=True):
+            st.switch_page("pages/valuation.py")
